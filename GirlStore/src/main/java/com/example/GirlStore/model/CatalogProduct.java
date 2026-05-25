409cf1c4ea3b6003ea053a16e7eaf1a60212024e
danidiaaz27 <U22302426@utp.edu.pe>
2026-05-25 16:24:42 -0500

    Primer commit de mi pagina web

---
+++ GirlStore/src/main/java/com/example/GirlStore/model/CatalogProduct.java
@@ -0,0 +1,4 @@
+package com.example.GirlStore.model;
+
+public record CatalogProduct(String name, String imagePath, int price) {
+}
---
+++ GirlStore/src/test/java/com/example/GirlStore/GirlStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GirlStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GirlStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
